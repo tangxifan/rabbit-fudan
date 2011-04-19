869f7e4fe53a70d2543dad9d7121a23067f2d407
zxhmike <zxhmike@6d1e6118-fd05-ef40-5f29-e32b2c8ffd12>
2011-04-19 14:18:53 +0000

    parser updated.

--- trunk/bread/src/utils/RBT_parser/test/test_parser.c
+++ trunk/bread/src/utils/RBT_parser/test/test_parser.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "RBT_parser.h"
 
 int main()
 {
 	char *filename;
-	if (NULL == (filename = malloc (1000 * sizeof(char)))){
+	char *temp;
+	char *author;
+
+	if (NULL == (filename = malloc (MAX_FILENAME_LENGTH * sizeof(char)))){
 		return -1;
 	}
 
-	if (NULL == (filename = RBT_find_device_file ("/home/mike/Projects/rabbit/parts/parts.lst", "10 Ω Resistor"))){
+	if (NULL == (author = malloc (BUF_SIZE * sizeof(char)))){
+		return -1;
+	}
+
+	if (NULL == (temp = malloc (BUF_SIZE * sizeof(char)))){
+		return -1;
+	}
+
+	if (NULL == (filename = RBT_find_device_file ("../../../../../parts/parts.lst", "10 Ω Resistor"))){
 		printf ("Error finding device.\n");
 		return -2;
 	}
+	
+	printf ("Filename found: %s\n", filename);
 
+	strcpy (temp, "../../../../../parts/parts/");
+	strcat (temp, filename);
+	strcpy (filename, temp);
+
+	if (NULL == (author = RBT_get_xml_node (filename, "/module/author"))){
+		printf ("Error locating XML node.\n");
+		return -3;
+	}
+
+	printf ("XML node found: %s\n", author);
+	
 	return 0;
 }
