f1d863fc1fd64145b73f89dd10faea0adef38649
zxhmike <zxhmike@6d1e6118-fd05-ef40-5f29-e32b2c8ffd12>
2011-05-24 08:48:39 +0000

    rbt_parser.c updated. Rest work to do: fill in pr_pin.

--- trunk/bread/src/rbt_parser/test/test_parser.c
+++ trunk/bread/src/rbt_parser/test/test_parser.c
@@ -3,11 +3,13 @@
 #include <string.h>
 #include "rbt_parser.h"
 
+int convert_pin_to_int (char*);
 int main()
 {
 	rbt_parse_netlist("simple_netlist.xml",
 		"../../../../parts/parts.lst",
 		"../../../../parts/parts/"
 	);	
+
 	return 0;
 }
