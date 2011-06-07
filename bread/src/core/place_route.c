9d24127ab01dcfc57241b95d6ce6e07dd6dfdd0a
tangxifan <tangxifan@6d1e6118-fd05-ef40-5f29-e32b2c8ffd12>
2011-06-07 13:17:01 +0000

    M bread/src/core/place_route.c

--- bread/src/core/place_route.c
+++ bread/src/core/place_route.c
@@ -3,3 +3,16 @@
 #include <rabbit_type.h>
 #include <place_route.h>
 
+int 
+place_route(IN int nvnet,
+            IN t_vnet* vnets,
+            IN int nmarco,
+            INOUT t_pr_marco* marcos,
+            INOUT t_bb_array* bb_array
+           )
+{
+  try_place(nvnet,vnets,nmarco,pr_marco,bb_array);
+  detail_place(nvnet,vnets,nmarco,pr_marco,bb_array);
+  try_route(nvnet,vnets,nmarco,pr_marco,bb_array);
+  return 1;
+}
