b4e5eb992421dd071f27b6e780549acf597e7967
tangxifan <tangxifan@6d1e6118-fd05-ef40-5f29-e32b2c8ffd12>
2011-06-08 07:49:29 +0000

    temp version --This line, and those below, will be ignored--
    M    core/bb_place.c
    A    core/bb_place.h

---
+++ trunk/bread/src/core/bb_place.h
@@ -0,0 +1,8 @@
+boolean
+detail_place(IN int nvnet,
+             IN t_vnet* vnets,
+             IN int nmarco,
+             INOUT t_pr_marco* pr_marco,
+             INOUT t_bb_array* bb_array
+            );
+
