a7e3ab528ef341017cc2fca1eca848c52c2093f9
zxhmike <zxhmike@6d1e6118-fd05-ef40-5f29-e32b2c8ffd12>
2011-06-14 13:29:15 +0000

    output updated.

--- trunk/bread/src/rbt_output.c
+++ trunk/bread/src/rbt_output.c
@@ -2,9 +2,13 @@
 rbt_output.c
 @Author: Mike Zou <zxhmike@gmail.com>
 */
+#ifndef __RBT_OUTPUT_H__
+#define __RBT_OUTPUT_H__
+
 #include <rabbit_type.h>
 #include <bb_type.h>
 #include <place_route.h>
+#include <rbt_parser.h>
 
 extern t_vnet *vnets;
 extern int vnets_length;
@@ -22,8 +26,7 @@ t_output_record *output_records;
 typedef s_output_pin{
 	int x;
 	int y;
-	char *x_str;
-	char *y_str;
+	char *pin_str;
 }t_output_pin;
 
 /*
@@ -59,11 +62,8 @@ rbt_output_record_init()
 			return -1;
 		
 		for (j = 0; j < output_records[i].pin_num; j++){
-			if (NULL == (output_records[i].pins[j].x_str = (char*)malloc (10 * sizeof (char))))
+			if (NULL == (output_records[i].pins[j].pin_str = (char*)malloc (10 * sizeof (char))))
 				return -1;
-			if (NULL == (output_records[i].pins[j].y_str = (char*)malloc (10 * sizeof (char))))
-				return -1;
-		}
 	}
 }
 
@@ -81,6 +81,20 @@ rbt_find_output_record
 
 
 /**
+ * function rbt_gen_pin_str
+ * Generate the pin expression that Qt can understand
+ */
+int
+rbt_gen_pin_str
+(char* pin_str, int x, int y)
+{
+	char* line_nos = "XY**ABCDE**FGHIJ**VW";
+
+	sprintf (pin_str, "%d%c%d", y/18, line_nos[y%18], y);
+	return 0;
+}
+
+/**
  * function rbt_output
  * Output the array information to a file for Qt use
  */
@@ -89,18 +103,46 @@ rbt_output
 (char* output_file)
 {
 	int x, y;
+	int i, j;
+	FILE *OUT;
+
+	t_output_record *record_cur;
+	t_output_pin *pin_cur;
 
 	for (x = 0;x < bb_array.height; x++)
 		for (y = 0; y < bb_array.width; y++){
 			/* Skip if nothing in a node */
 			if (
-				bb_array.bb_node.type == BLANK ||
-				bb_array.bb_node.status == FREE
+				bb_array.bb_node[x][y].type == BLANK ||
+				bb_array.bb_node[x][y].status == FREE
 			)
 				continue;
 
 			/* Record the device and pins */
+			if (NULL == (record_cur = rbt_find_output_record (bb_array.bb_node[x][y].pin->parent->device->name))){
+				return -2;
+			}
+
+			pin_cur = record_cur->pins[bb_array.bb_node[x][y].pin->pin_no];
+			pin_cur->x = x;
+			pin_cur->y = y;
+			rbt_gen_pin_str(pin_cur->pin_str, x, y);
 
 		}
+
+
+	/* Output */
+	if (NULL == (OUT = fopen (output_file, "w")))
+		return -2;
+
+	for (i = 0; i < marcos_length; i++){
+		fprintf (OUT, "%s", output_records[i].device_name);
+		for (j = 0; j < output_records[i].pin_num; j++)
+			fprintf(OUT, " %s", output_records[i].pins[j].pin_str);
+		fprintf(OUT, "\n");
+	}
+	close (OUT);
 	return 0;
 }
+
+#endif
